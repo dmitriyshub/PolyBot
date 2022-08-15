8464a2a4623ffc6d37916fca7cec89b954b01ca3
alexey <alexeymihaylovdev@gmail.com>
2022-08-15 23:09:55 +0300

    add terraform

--- terraform/bucket_s3.tf
+++ terraform/bucket_s3.tf
@@ -1,5 +1,6 @@
-resource "aws_s3_bucket" "b" {
+resource "aws_s3_bucket" "bucket" {
     bucket = var.bucket_name 
+  acl = "private"  # or can be "public-read"
 
   tags = {
     Name        = "EX1_aws_polybot"
@@ -7,7 +8,18 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  bucket = aws_s3_bucket.b.id
-  acl = var.acl_value   
+# Upload an object
+resource "aws_s3_object" "assassinatos" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "data/.envfile"
+  acl    = "private"
+  source = "D:/elements/project/PolyBot/.envfile"
+#  etag   = filemd5("D:/elements/project/PolyBot/.envfile")
+}
+resource "aws_s3_object" "telegram" {
+  bucket = aws_s3_bucket.bucket.id
+  key    = "data/.telegramToken"
+  acl    = "private"
+  source = "D:/elements/project/PolyBot/.telegramToken"
+#  etag   = filemd5("D:/elements/project/PolyBot/.envfile")
 }
