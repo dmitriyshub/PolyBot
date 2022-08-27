bc002618d4b011c726cee1d2864c2f0b3efe3ded
alexey <alexeymihaylovdev@gmail.com>
2022-08-27 19:32:40 +0300

    add terraform

--- bot.py
+++ bot.py
@@ -75,7 +75,7 @@ class YoutubeObjectDetectBot(Bot):
 
 
 if __name__ == '__main__':
-    with open('.telegramToken') as f:
+    with open('secret/.telegramToken') as f:
         _token = f.read()
 
     with open('config.json') as f:
